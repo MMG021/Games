a6ed80bc95c0276bcb57f950fb43f20508548483
Mohsin Munir <mohsingoraya313@outlook.com>
2022-01-24 21:19:05 +0500

    new

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import turtle as t
 playerAscore=0
 playerBscore=0
---
+++ practice.py
@@ -0,0 +1,17 @@
+from re import I
+
+
+a = 'Mohsin'
+jutt = 20
+j = True
+k = None
+print(jutt)
+print(a) 
+print(a)
+print(type(j))
+print(type(k))
+print(type(k))
+i=0
+while i<jutt:
+ print("*")
+ i += 3  
